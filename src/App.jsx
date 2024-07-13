bc3745c1ce53317b58d5f8ee2730f3c26f33933c
syukrontanzilah <syukrontanzilah@gmail.com>
2024-07-13 14:29:45 +0700

    add navbar and hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
+import { Hero, Highlights, Navbar } from "./components"
+import { appleImg, heroImg, watchImg } from "./utils"
 
 const App = () => {
 
   return (
-      <div>
-      <h1 className="text-3xl font-bold underline text-blue">
-      Hello world!
-    </h1>
-    <h1 className="text-3xl text-zinc">
-      halo lagi apa
-    </h1>
-    <div className="h-20 w-20 bg-blue">
-
-    </div>
-      </div>
+  <main className="bg-black">
+    <Navbar/>
+    <Hero/>
+    <Highlights/>
+    
+  </main>
   
   )
 }
---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import Hero from "./Hero"
+import Highlights  from "./Highlights"
+import Navbar from "./Navbar"
+
+export {
+    Hero,
+    Highlights,
+    Navbar
+}
