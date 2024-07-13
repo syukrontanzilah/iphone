e1139d5bea3b541ebb2e6c5efa882496b1ab1ef2
syukrontanzilah <syukrontanzilah@gmail.com>
2024-07-13 14:47:35 +0700

    update gsap

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,12 +25,18 @@ const Hero = () => {
     gsap.to("#hero", {
       opacity: 1,
       delay: 2,
+      yoyo:true,
+    })
+
+    gsap.to("#video",{
+      opacity:1
     })
 
     gsap.to("#cta", {
       opacity:1,
       y: -50,
-      delay: 2.5
+      delay: 2.5,
+      duration:2
     })
   },[])
 
@@ -39,7 +45,7 @@ const Hero = () => {
       <div className='h-5/6 w-full flex-center flex-col'>
         <p id="hero" className='hero-title'>iPhone 15 Pro</p>
         {/* video hero */}
-        <div className='md:w-10/12 w-9/12'>
+        <div id="video" className='opacity-0 md:w-10/12 w-9/12 transition-opacity'>
           <video className='pointer-events-none' autoPlay muted playsInline={true} key={videoSrc}>
             <source src={videoSrc} type='video/mp4'/>
           </video>
@@ -48,7 +54,7 @@ const Hero = () => {
 
       <div id='cta' className='flex flex-col items-center opacity-0 translate-y-20'>
         <a href='#highlights' className='btn'>Buy</a>
-        <p className='font-normal text-xl'>From $199/month or $999</p>
+        <p className='font-normal text-xl text-gray-100'>From $199/month or $999</p>
       </div>
       </section>
   )
