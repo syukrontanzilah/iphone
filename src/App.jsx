d66e12c766aff300343ae52de23c2f1d3316bca2
syukrontanzilah <syukrontanzilah@gmail.com>
2024-07-13 10:10:51 +0700

    config thailwind and add assets

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+const App = () => {
+
+  return (
+      <div>
+      <h1 className="text-3xl font-bold underline text-blue">
+      Hello world!
+    </h1>
+    <h1 className="text-3xl text-zinc">
+      halo lagi apa
+    </h1>
+    <div className="h-20 w-20 bg-blue">
+
+    </div>
+      </div>
+  
+  )
+}
+
+export default App
